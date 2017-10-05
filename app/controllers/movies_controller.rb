6b40fa6fd64bb95f33a425ec71ec58da24f36672
Stephanie Lam <lamstephanie32@gmail.com>
2017-10-05 15:28:32 +0000

    created controller and view for index and show, added CSS styling, completed 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie
+  end
+end
